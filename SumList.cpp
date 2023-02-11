70e46972f0deae8e78d1259ea2c3df1054fa34f3
Joshua Wiseman <addalooptoit@gmail.com>
2023-02-11 14:05:22 -0600

    Added Reversed List Function

--- SumList.cpp
+++ SumList.cpp
@@ -3,15 +3,24 @@
 
 using namespace std;
 
+// Returns the sum total of the List values
 int SumList(vector<int> v){
   int sum = 0;
   for(int i:v) sum+=i;
   return sum;
 }
+// Returns the product total of the List values
 int ProductList(vector<int> v){
   int sum = 1;
   for(int i:v) sum*=i;
   return sum;
+}
+
+// Returns all elements in the List, but in reverse order
+vector<int> ReversedList(vector<int> v){
+  vector<int> a;
+  for(int i = v.size()-1; i >= 0; i--) a.push_back(v[i]);
+  return a;
 }
 
 int main()
@@ -22,5 +31,10 @@ int main()
   while(cin>>number) list.push_back(number);
   printf("Sum of list = %d\n", SumList(list));
   printf("Product of list = %d\n", ProductList(list));
+  vector<int> reversedList = ReversedList(list);
+  printf("Reversed List Result\n");
+  for(int i = 0; i < reversedList.size(); i++){
+    printf("Element %d is value %d\n", i, reversedList[i]);
+  }
   return 0;
 }
